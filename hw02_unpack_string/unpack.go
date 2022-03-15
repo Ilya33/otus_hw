f7d5ca5484b065c88e256f22de788e474d77cf10
Ilya Pavlov <i.pavlov@id-network.ru>
2022-03-15 05:13:59 +0300

    golangci-lint fix

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -17,23 +17,24 @@ const (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
+	var result strings.Builder
+	var lastRune rune
+
+	state := StateNone
+
 	if !utf8.ValidString(s) {
 		return "", ErrInvalidString
 	}
 
-	var result strings.Builder
-
-	lastRune := rune(0)
-	state := StateNone
-
 	for _, currentRune := range s {
 		switch state {
 		case StateNone:
-			if currentRune == '\\' {
+			switch {
+			case currentRune == '\\':
 				state = HasSlash
-			} else if unicode.IsDigit(currentRune) {
+			case unicode.IsDigit(currentRune):
 				return "", ErrInvalidString
-			} else {
+			default:
 				lastRune = currentRune
 				state = HasRune
 			}
@@ -48,7 +49,7 @@ func Unpack(s string) (string, error) {
 
 		default:
 			if unicode.IsDigit(currentRune) {
-				//result.WriteString(strings.Repeat(string(lastRune), int(currentRune-'0'))) // it's better
+				// result.WriteString(strings.Repeat(string(lastRune), int(currentRune-'0'))) // it's better
 				times, _ := strconv.Atoi(string(currentRune)) // what would happen? it's a Digit 100%
 				result.WriteString(strings.Repeat(string(lastRune), times))
 
